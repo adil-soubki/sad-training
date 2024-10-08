bd3f458c5619739d4c82544052fc0527de161133
John Murzaku <jam@lissus.com>
2024-10-08 11:31:22 -0400

    Added factbank parser

--- src/data/fact_bank.py
+++ src/data/fact_bank.py
@@ -6,59 +6,89 @@ import datasets
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
+from head2span import get_text_span
+import json
+import re
+from typing import List, Dict
 
 from ..core.path import dirparent
 
+FB_TO_CLF = {
+    3.0: 'CT+',
+    2.0: 'PR+',
+    1.0: 'PR+',
+    0.0: 'UU',
+    -1.0: 'PR-',
+    -2.0: 'PR-',
+    -3.0: 'CT-',
+}
 
 FB_DIR = os.path.join(dirparent(os.path.realpath(__file__), 3), "data", "fb")
 
+def normalize_text(text: str) -> str:
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'\s+([,.!?;:(){}[\]%"\'’%])', r'\1', text)
+    
 
-@dataclasses.dataclass
-class Label:
-    source: str
-    target: str
-    value: str
+    contraction_suffixes = [
+        r"n['’]t",  # n't
+        r"['’]m",    # 'm
+        r"['’]s",    # 's
+        r"['’]re",   # 're
+        r"['’]ll",   # 'll
+        r"['’]ve",   # 've
+        r"['’]d",    # 'd
+        r"['’]em",   # 'em (optional, e.g., 'em for 'them')
+    ]
+    
+    pattern = r'\s+(' + '|'.join(contraction_suffixes) + r')\b'
+    text = re.sub(pattern, r'\1', text)
+    
+    text = re.sub(r'\$\s+', r'$', text)
+    
+    return text
 
-    def __post_init__(self):
-        assert self.value in ("false", "pfalse", "unknown", "ptrue", "true")
 
-
-def parse_labels(string: str) -> list[Label]:
-    ret = []
-    for label in string.split(";"):
-        label = label.strip()  # Remove whitespace.
-        assert label.startswith("(") and label.endswith(")")
-        ret.append(Label(*map(lambda s: s.strip(), label[1:-1].split(","))))
-    #  return ret
-    return list(map(dataclasses.asdict, ret))
+def process_jsonl(file_path: str) -> List[Dict]:
+    processed_data = []
+    with open(file_path, 'r') as infile:
+        for line in infile:
+            data = json.loads(line)
+            text = normalize_text(data['text'])
+            
+            for target in data['targets']:
+                span_text = normalize_text(target['span_text'])
+                full_span = get_text_span(span_text, text)
+                
+                if full_span:
+                    processed_data.append({
+                        'sentence': text,
+                        'original_head': span_text,
+                        'fb_span': full_span,
+                        'label': FB_TO_CLF[round(target['label'])]
+                    })
+    return processed_data
 
 
 def load_df() -> pd.DataFrame:
     ret = []
     for split in ("train", "dev", "test"):
-        ret.append(
-            pd.read_csv(os.path.join(FB_DIR, f"{split}.csv")).assign(split=split)
-        )
+        file_path = os.path.join(FB_DIR, f"{split}.jsonl")
+        processed_data = process_jsonl(file_path)
+        df = pd.DataFrame(processed_data)
+        df['split'] = split
+        ret.append(df)
+    
     df = pd.concat(ret).reset_index(drop=True)
-    df = df.assign(
-        input_text=df.input_text.str.replace("source target factuality: ", ""),
-        labels=list(map(parse_labels, df.target_text))
-    )
-    return df[["input_text", "target_text", "labels", "split"]]
-
+    return df[["sentence", "original_head", "fb_span", "label", "split"]]
 
 def load() -> datasets.Dataset:
     return datasets.Dataset.from_pandas(load_df(), preserve_index=False)
 
 
-def load_kfold(
-    fold: int, k: int = 5, seed: int = 42
-) -> datasets.DatasetDict:
-    assert fold >= 0 and fold <= k - 1
-    kf = KFold(n_splits=k, random_state=seed, shuffle=True)
+def load_kfold(fold: int, k: int = 5, seed: int = 42) -> datasets.DatasetDict: #XXX: FB has established split. Fold params are not used.
     fb = load()
-    train_idxs, test_idxs = list(kf.split(fb))[fold]
     return datasets.DatasetDict({
-        "train": fb.select(train_idxs),
-        "test": fb.select(test_idxs),
+        "train": fb.filter(lambda x: x['split'] == 'train'), #XXX: Not using dev yet.
+        "test": fb.filter(lambda x: x['split'] == 'test'),
     })
---
+++ src/data/head2span.py
@@ -0,0 +1,89 @@
+import spacy
+import re
+
+'''
+Head2Span stuff
+'''
+def load_spacy(spacy_model="en_core_web_sm"):
+    global nlp
+    nlp = spacy.load(spacy_model, exclude=["lemmatizer", "ner", "textcat"])
+
+
+load_spacy()
+
+
+def get_final_span(syntactic_head_token, fb_head_token):
+    # mention subtree vs children distinction in meeting!
+    syntactic_head_subtree = list(syntactic_head_token.subtree)
+
+    relevant_tokens = []
+
+    for token in syntactic_head_subtree:
+        if token.dep_ in ['cc', 'conj'] and token.i > fb_head_token.i:
+            break
+        relevant_tokens.append(token)
+
+    left_edge = relevant_tokens[0].idx
+    right_edge = relevant_tokens[-1].idx + len(relevant_tokens[-1].text)
+
+    return left_edge, right_edge
+
+
+def get_head_span(head_token_offset_start, head_token_offset_end, doc):
+    fb_head_token = doc.char_span(head_token_offset_start, head_token_offset_end,
+                                  alignment_mode='expand')[0]
+
+    # when above target, eliminate CC or CONJ arcs
+    # if on non-FB-target verb mid-traversal, DO take CC or CONJ arcs
+    # if hit AUX, don't take CC or CONJ - don't worry for now
+    if fb_head_token.dep_ == 'ROOT':
+        syntactic_head_token = fb_head_token
+    else:
+        syntactic_head_token = None
+        ancestors = list(fb_head_token.ancestors)
+        ancestors.insert(0, fb_head_token)
+
+        if len(ancestors) == 1:
+            syntactic_head_token = ancestors[0]
+        else:
+            for token in ancestors:
+                if token.pos_ in ['PRON', 'PROPN', 'NOUN']:
+                    syntactic_head_token = token
+                    break
+                elif token.pos_ in ['VERB', 'AUX']:
+                    syntactic_head_token = token
+                    break
+
+            if syntactic_head_token is None:
+                for token in ancestors:
+                    if token.pos_ == 'NUM':
+                        syntactic_head_token = token
+                        break
+
+    # postprocessing for CC and CONJ -- exclude child arcs with CC or CONJ
+    span_start, span_end = get_final_span(syntactic_head_token, fb_head_token)
+
+    return span_start, span_end
+
+
+def get_span_indices(word, text):
+    try:
+        return re.search(word, text, flags=re.IGNORECASE).span()
+    except:
+        return False
+
+
+def get_text_span(target, text):
+
+    doc = nlp(text)
+
+    span_ixs = get_span_indices(target, text)
+
+    try:
+        s, e = get_head_span(*span_ixs, doc)
+        span = text[s:e]
+        return span
+    except:
+        return False
+
+
